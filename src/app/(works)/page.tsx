2611056536899678fa5aac2797e90aba99bf617a
Seo jeong Kim <jennysujukim@gmail.com>
2023-12-29 23:48:26 -0800

    chore: update files

--- src/app/(works)/page.tsx
+++ src/app/(works)/page.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useState, useEffect } from 'react'
 // components
 import MainNav from '@/app/components/MainNav'
 import ArticleDescriptions from '../components/ArticleDescriptions'
@@ -5,14 +8,19 @@ import ArticleImgs from '../components/ArticleImgs'
 // styles
 import styles from './worksPage.module.css'
 
-
 export default function WorksPage() {
+
+  const [navWidth, setNavWidth] = useState<number>(200);
+
+
   return (
     <main className={styles.main}>
       <div className={styles.imgsContainer}>
         <ArticleImgs />
       </div>
-      <MainNav />
+      <div className={styles.navContainer} style={{ width: `${navWidth}px` }} >
+        <MainNav />
+      </div>
       <div className={styles.descriptionsContainer}>
         <ArticleDescriptions />
       </div>
