665174ae763bed7f2ff185b1fcbf607677d2c8a3
Seo jeong Kim <jennysujukim@gmail.com>
2024-04-10 13:20:26 -0700

    feat: apply responsive component for homepage Hero

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -5,6 +5,7 @@ import useResponsive from '@/app/utils/useResponsive';
 import { BreakpointType } from '@/types/enums/BreakpointType';
 // components
 import MatterJSBridgeMobile from "../MatterJSBridgeMobile";
+import MatterJSBridgeTablet from "../MatterJSBridgeTablet";
 import MatterJSBridgeLaptop from "../MatterJSBridgeLaptop";
 import ResizeListener from '../ResizeListener';
 import Loading from '../Loading';
@@ -33,7 +34,7 @@ export default function Hero() {
           (<MatterJSBridgeLaptop key={forceRerender ? 'forceRerender' : 'normal'} />)
           :
           isResponsive === BreakpointType.Tablet || isResponsive === BreakpointType.TabletLg ?
-          (<MatterJSBridgeMobile key={forceRerender ? 'forceRerender' : 'normal'} ratio={0.11} />)
+          (<MatterJSBridgeTablet key={forceRerender ? 'forceRerender' : 'normal'} ratio={0.11} />)
           :
           (<MatterJSBridgeMobile key={forceRerender ? 'forceRerender' : 'normal'} ratio={0.2} />)
         )
