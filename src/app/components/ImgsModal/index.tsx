4ab25424fe6172496dd1cea61402d3698cab683f
Seo jeong Kim <jennysujukim@gmail.com>
2024-04-15 12:00:48 -0700

    feat: create modal component and apply responsive design

--- src/app/components/ImgsModal/index.tsx
+++ src/app/components/ImgsModal/index.tsx
@@ -1,5 +1,28 @@
-export default function ImgsModal() {
+"use client"
+
+import Image from 'next/image'
+import React from 'react'
+// styles
+import styles from './ImgsModal.module.css'
+
+type ImgsModal = {
+  src: string;
+  title: string;
+  handleClose: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default function ImgsModal({ src, title, handleClose }: ImgsModal) {
+
   return (
-    <div>ImgsModal</div>
+    <div 
+      className={styles.container}
+      onClick={() => handleClose(false)}
+    >
+      <Image 
+        src={src}
+        alt={title}
+        className={styles.image}
+      />
+    </div>
   )
 }
