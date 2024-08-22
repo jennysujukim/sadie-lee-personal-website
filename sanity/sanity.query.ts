b1ffc6cc4fc4d48dd14279fb940875e79dfb8395
Seo jeong Kim <jennysujukim@gmail.com>
2024-08-21 22:17:09 -0700

    feat: setup about data schema and fetch data

--- sanity/sanity.query.ts
+++ sanity/sanity.query.ts
@@ -1,18 +1,32 @@
 import { groq } from "next-sanity";
 import client from "./sanity.client";
 
-export async function getWork() {
-  return client.fetch(
-    groq`*[_type == "work"]{
-      id,
-      title,
-      slug { current },
-      type,
-      year,
-      keywords,
-      materials,
-      descriptions,
-      "images": images[].asset->url
-    }`
-  );
+const workQuery = groq`*[_type == "work"]{
+  id,
+  title,
+  slug { current },
+  type,
+  year,
+  keywords,
+  materials,
+  descriptions,
+  "images": images[].asset->url
+}`
+
+const aboutQuery = groq`*[_type == "about"]{
+  title,
+  main,
+  experience,
+  education,
+  awardsAndEvents,
+  exhibition
+}`
+
+export async function getData() {
+  const [work, about] = await Promise.all([
+    client.fetch(workQuery),
+    client.fetch(aboutQuery),
+  ]);
+
+  return { work, about };
 }
--- src/app/context/WorkContext.tsx
+++ src/app/context/WorkContext.tsx
@@ -2,7 +2,7 @@
 
 import React, { createContext, useState, useEffect } from "react";
 import { WorkType } from '../../types/models/Work';
-import { getWork } from '../../../sanity/sanity.query'
+import { getData } from '../../../sanity/sanity.query'
 
 type WorkContextType = {
   works: WorkType[];
@@ -24,10 +24,11 @@ export const WorkContextProvider = ({children}: WorkContextProviderProps) => {
   const [ works, setWorks ] = useState<WorkType[]>([])
 
   useEffect(() => {
-    getWork().then(data => { 
-      const sortRes = data.sort((a: { id: number; }, b: { id: number; }) => a.id - b.id)
-      setWorks(sortRes)
-    }).catch((error) => console.error(error))
+    console.log(getData())
+    // getWork().then(data => { 
+    //   const sortRes = data.sort((a: { id: number; }, b: { id: number; }) => a.id - b.id)
+    //   setWorks(sortRes)
+    // }).catch((error) => console.error(error))
   }, [])
 
     return (
