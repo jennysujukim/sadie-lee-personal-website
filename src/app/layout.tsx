b7e0fc35170264105d3ab441a0609939129e789f
Seo jeong Kim <jennysujukim@gmail.com>
2024-08-24 17:30:22 -0700

    fix: open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Sadie Lee : Graphic designer based in London',
     description: 'Sadie Lee is a graphic designer with a strong foundation in art education, further enriched by advanced studies in Interaction Design and a Master’s degree in Graphic Media Design from LCC (UAL). Her interdisciplinary background empowers her to create innovative and impactful designs that effectively tackle a wide range of creative challenges.',
-    images: ['/big-circle.png'],
+    images: ['https://www.sadielee-design.com/big-circle.png'],
     url: 'https://www.sadielee-design.com/',
     type: 'website',
     locale: 'en_GB'
