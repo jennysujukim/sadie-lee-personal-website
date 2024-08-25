58d8585bd59e8441a58471598183584ca8c28416
Seo jeong Kim <jennysujukim@gmail.com>
2024-08-24 17:09:58 -0700

    feat: modify mainNav and styles

--- src/app/components/NavLink/index.tsx
+++ src/app/components/NavLink/index.tsx
@@ -10,26 +10,35 @@ type NavLinkProps = {
   link: string;
   text: string;
   isHome?: boolean;
+  isHighlight?: boolean;
 }
 
-export default function NavLink({ link, text, isHome }: NavLinkProps) {
+export default function NavLink({ link, text, isHome, isHighlight }: NavLinkProps) {
+
   return (
     <Link
       href={link}
       className={styles.link}
     >
-      <div className={styles.checkboxContainer}>
+      <div className={styles.checkboxContainer}>        
         <Image 
           src={check} 
           alt="Check icon"
-          className={isHome ? `${styles.check} ${styles.isHome}` : styles.check}
+          className={isHighlight ? `${styles.check} ${styles.highlight}` : styles.check}
         />
+        {isHome &&
+          <Image 
+            src={check} 
+            alt="Check icon"
+            className={isHome ? `${styles.check} ${styles.isHome}` : styles.check}
+          />
+        }
         <Image 
           src={checkbox} 
           alt="Checkbox icon"
         />
       </div>
-      <p className={isHome ? `${styles.linkText} ${styles.isHome}`: styles.linkText}>
+      <p className={isHome ? `${styles.linkText} ${styles.isHome}`: isHighlight ? `${styles.linkText} ${styles.highlight}` : styles.linkText }>
         {text}
       </p>
     </Link>
