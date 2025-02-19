89cc02b815acd43b3c2da56408f3bf43a7f91145
Seo jeong Kim <jennysujukim@gmail.com>
2025-02-19 08:55:32 -0800

    feat: update dataContext for filter

--- src/app/context/DataContext.tsx
+++ src/app/context/DataContext.tsx
@@ -8,6 +8,9 @@ import { getData } from '../../../sanity/sanity.query';
 type DataContextType = {
   works: WorkType[];
   about: AboutType | null;
+  setSelectedWorks: (works: WorkType[]) => void;
+  selectedWorks: WorkType[];
+  filterWorks: (filter: string) => void;
 };
 
 type DataContextProviderProps = {
@@ -16,7 +19,10 @@ type DataContextProviderProps = {
 
 const defaultContextValue: DataContextType = {
   works: [],
-  about: null
+  about: null,
+  setSelectedWorks: () => {},
+  selectedWorks: [],
+  filterWorks: () => {}
 };
 
 export const DataContext = createContext(defaultContextValue)
@@ -26,18 +32,29 @@ export const DataContextProvider = ({children}: DataContextProviderProps) => {
 
   const [ works, setWorks ] = useState<WorkType[]>([])
   const [ about, setAbout ] = useState<AboutType | null>(null)
+  const [selectedWorks, setSelectedWorks] = useState<WorkType[]>([]);
 
   useEffect(() => {
     getData().then(data => { 
       const sortWorkRes = data.work.sort((a: { id: number; }, b: { id: number; }) => a.id - b.id)
       setWorks(sortWorkRes)
+
       const aboutRes = data.about.find((data: AboutType) => data.title === "Updated")
       setAbout(aboutRes)
     }).catch((error) => console.error(error))
   }, [])
 
+
+  const filterWorks = (filter: string) => {
+    if (filter === 'all') {
+      setSelectedWorks(works)
+    } else {
+      setSelectedWorks(works.filter((work: WorkType) => work.filter === filter))
+    }
+  }
+
     return (
-        <DataContext.Provider value={{ works, about }}> 
+        <DataContext.Provider value={{ works, about, selectedWorks, setSelectedWorks, filterWorks }}> 
             {children}
         </DataContext.Provider>
     )
